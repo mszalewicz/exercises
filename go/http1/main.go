08752134f5ceaf9c6cc242ac1f5e2fcb229c47db
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-01-15 17:01:00 +0100

    http server kata

---
+++ go/http1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	address := "127.0.0.1:8080"
+	http.HandleFunc("/", mainPage)
+	http.ListenAndServe(address, nil)
+}
+
+func mainPage(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(responseWriter, "main page")
+}
