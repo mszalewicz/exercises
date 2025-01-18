b69e9964fbc147a4099fd0b202a1ed9e5c2aa8ac
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-01-18 13:08:02 +0100

    context kata

---
+++ go/kata/context6/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	responseChannel := make(chan string)	
+	ctx, cancel := context.WithTimeout(context.Background(), 100 * time.Millisecond)
+	defer cancel()
+
+	go ThirdPartyQuery(ctx, responseChannel)
+
+	select {
+		case <-ctx.Done():
+			fmt.Println("Third party query timed out.")
+		case response := <-responseChannel:
+			fmt.Println(response)
+	}
+}
+
+func ThirdPartyQuery(ctx context.Context, responseChannel chan string) {
+	for {
+		select {
+			case <-ctx.Done():
+				return
+			default:
+				time.Sleep(150 * time.Millisecond)
+				responseChannel <-"Third party info: {...}"
+		}
+	}		
+}
