dc92773ed1b3d9b9be132387385bbf4d2b08c819
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-01-19 12:51:57 +0100

    testing kata

---
+++ go/kata/testing4/unit_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	functionName := "TestAdd"
+
+	testCases := []struct {
+		a        int
+		b        int
+		expected int
+	}{
+		{1, 4, 9},
+	}
+
+	for index, test := range testCases {
+		t.Run(fmt.Sprintf("%s case %d", functionName, index), func(t *testing.T){
+			if result := add(test.a, test.b); result != test.expected {
+				t.Fatalf("%s case number %d failed - expected: %d | result: %d", functionName, index, test.expected, result)
+			}
+		})
+	}
+}
