29accd353d9a66fc8da9af744049601af2a16cb2
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-01-19 17:33:52 +0100

    leet code 75: problem 6

---
+++ leetcode/leetcode_75/6/unit_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	functionName := "Test"
+	testCases := []struct {
+		text     string
+		expected string
+	}{
+		{"the sky is blue", "blue is sky the"},
+		{"  hello world  ", "world hello"},
+		{"a good   example", "example good a"},
+	}
+
+	for index, test := range testCases {
+		t.Run(fmt.Sprintf("%s case %d", functionName, index), func(t *testing.T) {
+			if result := reverseWords(test.text); result != test.expected {
+				t.Fatalf("%s case number %d failed - expected: %s | result: %s", functionName, index, test.expected, result)
+			}
+		})
+	}
+}
