9e5319e28c1510c2844d0d9aebc09e9d1999db10
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-03-31 15:13:20 +0200

    aoc elixir 2 part 2

--- elixir/kata/advent_of_code/02/main.exs
+++ elixir/kata/advent_of_code/02/main.exs
@@ -5,21 +5,44 @@ if status == :error do
   System.halt(1)
 end
 
-result
-|> String.split("\n", trim: true)
-|> Enum.map(fn line ->
-  line
-  |> String.split()
-  |> Enum.map(&String.to_integer/1)
-end)
-|> Enum.count(fn numbers ->
-  consequtive_tuples = Enum.chunk_every(numbers, 2, 1, :discard)
-  increasing = consequtive_tuples |> Enum.all?(fn [a, b] -> a < b end)
-  decreasing = consequtive_tuples |> Enum.all?(fn [a, b] -> a > b end)
+defmodule AOC do
+  def isSafe?(list) do # does list meet the conditions ?
+    consequtive_tuples = Enum.chunk_every(list, 2, 1, :discard)
+    increasing = consequtive_tuples |> Enum.all?(fn [a, b] -> a < b end)
+    decreasing = consequtive_tuples |> Enum.all?(fn [a, b] -> a > b end)
 
-  valid_differences = consequtive_tuples |> Enum.all?(fn [a, b] -> abs(a - b) in 1..3 end)
+    valid_differences = consequtive_tuples |> Enum.all?(fn [a, b] -> abs(a - b) in 1..3 end)
 
-  (increasing or decreasing) and valid_differences
-end)
+    (increasing or decreasing) and valid_differences
+  end
+
+  def isSafeWithOneBad?(list) do # does list meet the conditions if we delete one value ?
+    if isSafe?(list) do
+      true
+    else
+      Enum.any?(0..(length(list) - 1), fn i ->
+        list |> List.delete_at(i) |> isSafe?()
+      end)
+    end
+  end
+end
+
+# Prepare lists of integers
+integers =
+  result
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn line ->
+    line
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end)
+
+#  Part 1
+integers
+|> Enum.count(&AOC.isSafe?/1)
 |> then(fn result -> IO.puts("Part 1: #{result}") end)
 
+# Part 2
+integers
+|> Enum.count(&AOC.isSafeWithOneBad?/1)
+|> then(fn result -> IO.puts("Part 2: #{result}") end)
