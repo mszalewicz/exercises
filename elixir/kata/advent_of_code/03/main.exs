30c08d1a1e12aea58a78d1fcf6b10590011f1baf
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-06-08 17:35:21 +0200

    aoc elixir 3 part 1

--- elixir/kata/advent_of_code/03/main.exs
+++ elixir/kata/advent_of_code/03/main.exs
@@ -14,3 +14,24 @@ Regex.scan(~r/mul\((\d+),(\d+)\)/, file)
 |> Enum.map(fn [_, num1, num2] -> String.to_integer(num1) * String.to_integer(num2) end)
 |> Enum.reduce(0, fn x, acc -> x + acc end)
 |> then(fn result -> IO.puts("Part 1: #{result}") end)
+
+firstMul =
+  Regex.run(~r/mul\((\d+),(\d+)\)/, file)
+  |> then(fn [_, num1, num2] -> String.to_integer(num1) * String.to_integer(num2) end)
+
+  # |> then(fn result -> IO.puts("Part 2: #{result}") end)
+
+restMuls =
+  Regex.scan(~r/do\(\).*?mul\((\d+),(\d+)\)/, file)
+  |> IO.inspect()
+  |> Enum.map(fn [_, num1, num2] -> String.to_integer(num1) * String.to_integer(num2) end)
+  |> Enum.reduce(0, fn x, acc -> x + acc end)
+
+result2 = firstMul + restMuls
+
+IO.puts("Part 2: #{result2}")
+
+IO.inspect(firstMul)
+
+# 82733683
+# 8980089
