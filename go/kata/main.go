ef3ff827726174eadcd147a2a0c1162d220b69d1
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-11-09 21:44:05 +0100

    concurrency kata

---
+++ go/kata/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+	counter := 0
+
+	for i := range 1000 {
+		wg.Add(1)
+
+		go func(i int) {
+			defer wg.Done()
+
+			rndTime := time.Duration(rand.Int32N(300))*time.Millisecond + 150*time.Millisecond
+			time.Sleep(rndTime)
+
+			mutex.Lock()
+			counter++
+			mutex.Unlock()
+
+			fmt.Println(time.Now())
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutines done:", counter)
+}
