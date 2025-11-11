956dc7aff4df56180a98b1ca7b22fb836c7f46ff
Maciej Szalewicz <m.szalewicz@gmail.com>
2025-11-11 18:46:55 +0100

    fizzbuzz kata

---
+++ go/kata/fizzbuzz6/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	mapping := map[int]string{
+		3: "fizz",
+		5: "buzz",
+		7: "other",
+	}
+
+	numberText := os.Args[1]
+	number, err := strconv.Atoi(numberText)
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	for key, value := range mapping {
+		if number%key == 0 {
+			fmt.Print(value, " ")
+		}
+	}
+
+	fmt.Println()
+}
